2f4219a72c9fd3b6c2c87fac52ff07a757f745a0
Kyrie <yuzhezhong0117@qq.com>
2026-08-03 16:24:54 +0800

    feat(practice): record LeetCode 1406 acceptance

---
+++ problems/leetcode/1406/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        f = [-inf] * n + [0]
+
+        for i in range(n - 1, -1, -1):
+            s = 0
+            for j in range(i, min(i + 3, n)):
+                s += stoneValue[j]
+                f[i] = max(f[i], s - f[j + 1])
+
+        if f[0] == 0:
+            return "Tie"
+        else:
+            return "Alice" if f[0] > 0 else "Bob"
+
